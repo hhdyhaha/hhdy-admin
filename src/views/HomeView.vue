4ce9dc131d546f0abcba091dfff7e4dd658590f1
hzdgzz <1164549585@qq.com>
2024-04-06 00:50:52 +0800

    tailwindcss 基本使用

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="common-layout">
-    <el-container>
-      <el-aside width="200px">Aside</el-aside>
-      <el-container>
-        <el-header>Header</el-header>
-        <el-main>Main</el-main>
+  <div class="h-full">
+    <el-container class="h-full">
+      <el-aside width="20%" class="bg-blue-100">
+        <div class="w-full h-[60px] bg-amber-500 flex justify-center items-center">
+          侧边栏
+        </div>
+      </el-aside>
+      <el-container class="w-4/5">
+        <el-header class="flex justify-start items-center bg-blue-50">
+          <div class="w-1/2 h-full bg-blue-300 flex items-center">
+            首页
+          </div>
+          <div class="w-1/2 h-full bg-amber-500 flex justify-end items-center">
+            用户名
+          </div>
+        </el-header>
+        <el-main class="w-full h-full bg-blue-200">
+          <div class="bg-amber-100">
+            Hello world!
+          </div>
+        </el-main>
       </el-container>
     </el-container>
   </div>
@@ -14,11 +29,7 @@
 </script>
 
 <style lang="scss" scoped>
-.common-layout {
-  height: 100%;
-
-  .el-container {
-    height: 100%;
-  }
+.el-header {
+  height: 60px !important;
 }
 </style>
